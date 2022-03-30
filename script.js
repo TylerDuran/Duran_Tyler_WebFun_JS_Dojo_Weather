ec6d867afbd328619d6b5abd44883fb385830cd8
Tyler Duran <tymduran@gmail.com>
2022-03-29 20:24:14 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,18 @@
+/*******************************************/
+/* Alert */
+
+function alertTheUser () {
+    alert("Loading weather report...")
+}
+
+/*******************************************/
+/* Remove Cookie */
+
+function removeCookie(){
+    var getRid = document.querySelector("#rmcookie")
+    getRid.remove()
+}
+
+/*******************************************/
+/* Convert Temp */
+
